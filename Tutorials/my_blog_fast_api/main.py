ffe2c9b2e88aee9c3ae7a1fce35327108e0a63d5
manuelaceron <mafe-cv@hotmail.com>
2026-03-18 07:33:30 +0100

    Add blog endpoints

--- Tutorials/my_blog_fast_api/main.py
+++ Tutorials/my_blog_fast_api/main.py
@@ -1,7 +1,31 @@
 from fastapi import FastAPI
+from data.blog_entries import posts
+from fastapi.responses import HTMLResponse
 
+""" 
+FastAPI Supports sync and async functions
+
+fastapi dev main.py -> converts my functions to json (or HTML if needed)
+ - dev: better for debugging, re starst server after changes
+ - run: optimized for production
+ 
+ Automatic route documentation generation in http://127.0.0.1:8000/doc or ../redoc
+ 
+ Recommendation:
+  - Have separation between API routes (json response: for programmatic purposes) 
+    and HTML routes (for human readability) -> Use include in schema to hide from documentation HTML responses
+  - Decorator stacking, in the example, to give many routes to the same function
+"""
 app = FastAPI()
 
 
-def test():
-    print("hello")
+@app.get("/", response_class=HTMLResponse, include_in_schema=False)
+@app.get("/posts", response_class=HTMLResponse, include_in_schema=False)
+def home():
+    # return {"message": "Welcome to my blog!"}
+    return f"<h1> Welcome to my blog! </h1>"
+
+
+@app.get("/api/posts")
+def get_posts():
+    return posts
